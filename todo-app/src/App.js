dc9c8678ae54bfab36aec47a812a0e52a0fc636f
Joshua Lo <joshua.braeden.lo@gmail.com>
2023-08-28 17:11:22 -0400

    Serverless ToDo List
    serverless react todo list

---
+++ todo-app/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState('');
+
+	const handleAddTask = () => {
+		if (newTask.trim() !== '') {
+			fetch('https://jlctu82hwc.execute-api.us-west-2.amazonaws.com/default',{
+				method: 'POST',
+				headers:{
+					'content-type':'application/json'
+				},
+				body:JSON.stringify({ task: newTask}),
+			})
+			.then(response => response.json())
+			.then(data => {
+				if (data.success) {
+					setTasks([...tasks, newTask]);
+					setNewTask('');
+				} else {
+					console.error('Error Adding Task');
+				}
+			})
+			.catch(error => {
+				console.error('Error Adding Task', error);
+			});
+		}
+
+	};
+
+  return (
+    <div className="App">
+	  <h1>To-Do List</h1>
+	  <div className='input-containers'>
+	  <input 
+	  type= 'text'
+	  placeholder='Enter A New Task...'
+	  value={newTask}
+	  onChange={(e) => setNewTask(e.target.value)}
+	  />
+	  <button onClick = {handleAddTask}>Add</button>
+	  </div>
+	  <ul className='task-list'>
+	  {tasks.map((task, index) => (
+		  <li key={index}>{task}</li>
+	  ))}
+		  </ul>
+		  </div>
+
+
+  );
+}
+
+export default App;
