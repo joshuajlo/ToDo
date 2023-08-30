ab3bfda45c258f0c2fdc0f35c58d399b262ce115
Joshua Lo <joshua.braeden.lo@gmail.com>
2023-08-30 03:29:50 -0400

    Added edit and delete task

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
 	const [tasks, setTasks] = useState([]);
 	const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    //fetch tasks from api and update the tasks state
+
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch('api');
+      const data = await response.json();
+      if (data.success){
+        setTasks(data.tasks);
+      } else {
+        console.error('Error fetching tasks');
+      }
+    } catch (error) {
+      console.error('Error Fetching tasks', error);
+    }
+  };
 
 	const handleAddTask = () => {
 		if (newTask.trim() !== '') {
@@ -30,6 +50,46 @@ function App() {
 
 	};
 
+	const handleEditTask = (taskId, newTaskText) => {
+		fetch('api', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body:JSON.stringify({task:newTaskText}),
+		})
+.then(response => response.json())
+.then(data => {
+	if (data.success) {
+		//refresh task list
+	} else {
+		console.error('Error updating task');
+	}
+})
+.catch(error => {
+	console.error('Error updating task', error);
+});
+};
+
+const handleRemoveTask = (taskId) => {
+	if (window.confirm('Are you sure you want to delete this task?')) {
+		fetch('api', {
+			method: 'DELETE',
+		})
+		.then(response => response.json())
+		.then(data => {
+		if (data.success) {
+			//refresh task list
+		} else {
+			console.error('Error deleting task');
+		} 
+		})
+		.catch(error => {
+			console.error('Error deleting task', error);
+		});
+	}
+};
+
   return (
     <div className="App">
 	  <h1>To-Do List</h1>
@@ -43,13 +103,30 @@ function App() {
 	  <button onClick = {handleAddTask}>Add</button>
 	  </div>
 	  <ul className='task-list'>
-	  {tasks.map((task, index) => (
-		  <li key={index}>{task}</li>
+	  {tasks.map((task) => (
+		  <li key={task.id}>
+      {tasks.text}
+      <button
+      className ='edit-button'
+      onClick={() => {
+        const newTaskText = prompt('Edit Task:', task.text);
+        if (newTaskText !== null && newTaskText.trim() !== '') {
+          handleEditTask(task.id, newTaskText);
+        }
+      }}
+      >
+      Edit
+      </button>
+      <button
+      className ='delete-button'
+      onClick={() => handleRemoveTask(task.lid)}
+      >
+      Delete
+      </button>
+      </li>
 	  ))}
 		  </ul>
 		  </div>
-
-
   );
 }
 
